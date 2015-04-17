6ded76e674a4360df4f25bf1254ff4acf3c8a3e9
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-04-17 00:55:50 -0400

    Enhance the Integriforce helper script.

--- util/integriforce.zsh
+++ util/integriforce.zsh
@@ -30,7 +30,7 @@ EOF
 	str="${str}${str2}"
 
 	if [ ! -x ${executable} ]; then
-		echo "${executable} does not exist!" 1>2
+		echo "${executable} does not exist!" >&2
 		return 1
 	fi
 
@@ -67,18 +67,76 @@ function do_directory() {
 	done
 }
 
+function secadm_check_dups() {
+	config=${1}
+	res=0
+	tmpfile=$(mktemp)
+
+	if [ ${#config} -eq 0 ]; then
+		echo "USAGE: ${0} /path/to/config" >&2
+		return 1
+	fi
+
+	# Assume the Integriforce section is the last section.
+	# Trim what we look at to only the Integriforce section.
+	
+	line=$(grep -niF 'integriforce' ${config} | awk '{print $1;}' \
+	    | sed 's/://')
+	if [ ${#line} -eq 0 ]; then
+		rm -f ${tmpfile}
+		return 0
+	fi
+
+	foreach file in $(sed -e 1,${line}d ${config} | grep -i path \
+	    | awk '{print $2;}' | sed 's/[",]//g'); do
+		printout=1
+
+		while read tfile; do
+			if [ ${tfile} = ${file} ]; then
+				printout=0
+			fi
+		done < ${tmpfile}
+
+		if [ ${printout} -eq 1 ]; then
+			count=$(sed -e 1,${line}d ${config} \
+			    | grep -iw ${file} | uniq -c \
+			    | awk '{print $1;}')
+
+			if [ $((${count} + 0)) -gt 1 ]; then
+				echo ${file} >> ${tmpfile}
+				echo "${file} has ${count} entries"
+				res=$((${res} + 1))
+			fi
+		fi
+	done
+
+	rm -f ${tmpfile}
+
+	return ${res}
+}
+
 if [ ${#@} -eq 0 ]; then
-	echo "USAGE: ${0} file"
+	echo "USAGE: ${0} [-c <config] [-d <directory>] [-f <file>]"
+	echo "USAGE: ${0} [-f /path/to/file] [-d /path/to/directory] [-c /path/to/config]" >&2
+	echo "ARGUMENTS:"
+	echo "    -c <file>\tCheck the configuration file for duplicate Integriforce entries."
+	echo "             \tThe Integriforce section must be the last section of the config file."
+	echo "    -d <dir>\tOutput Integriforce configuration for executable files in <dir> and their dependencies."
+	echo "    -f <file>\tOutput Integriforce configuration for <file> and its dependencies."
 	exit 1
 fi
 
-while getopts 'f:d:' o; do
+while getopts 'c:d:f:' o; do
 	case ${o} in
-		f)
-			do_executable ${OPTARG}
+		c)
+			secadm_check_dups ${OPTARG}
+			exit ${?}
 			;;
 		d)
 			do_directory ${OPTARG}
+			;;
+		f)
+			do_executable ${OPTARG}
 			;;
 		*)
 			;;
