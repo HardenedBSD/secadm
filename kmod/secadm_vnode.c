082906876898260f699a1a953c555eb70b56f81a
Oliver Pinter <op@hardenedbsd.org>
2015-05-03 18:17:34 +0200

    Disable integriforce on 10-STABLE
    Signed-off-by: Oliver Pinter <op@hardenedbsd.org>

--- kmod/secadm_vnode.c
+++ kmod/secadm_vnode.c
@@ -108,9 +108,11 @@ secadm_vnode_check_exec(struct ucred *ucred, struct vnode *vp,
 			case aslr_disabled:
 				flags |= PAX_NOTE_NOASLR;
 				break;
+#if __FreeBSD_version > 1100000
 			case integriforce:
 				err = do_integriforce_check(rule, &vap, imgp->vp, ucred);
 				break;
+#endif
 			default:
 				break;
 			}
@@ -197,12 +199,14 @@ secadm_vnode_check_open(struct ucred *ucred, struct vnode *vp,
 		    rule->sr_mount))
 			continue;
 
+#if __FreeBSD_version > 1100000
 		if (lookup_integriforce_feature(rule) != NULL) {
 			printf("Info: A process tried to modify a "
 			    "file protected by a secadm rule. "
 			    "Returning EPERM.\n");
 			res=EPERM;
 		}
+#endif
 		break;
 	}
 	SPL_RUNLOCK(entry, tracker);
