da70f4cb3316a92b050e433b9102280b39b3137b
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2017-09-05 08:40:04 +0200

    HBSD: same as the previous commit, but for 10 and 11
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- kmod/secadm_vnode.c
+++ kmod/secadm_vnode.c
@@ -47,7 +47,9 @@ static int
 secadm_pax_elf(struct image_params *imgp, pax_flag_t flags)
 {
 
-#if (__HardenedBSD_version < 1300000 &&__HardenedBSD_version > 1200052)
+#if (__HardenedBSD_version < 1300000 && __HardenedBSD_version > 1200052) || \
+    (__HardenedBSD_version < 1200000 && __HardenedBSD_version > 1100048) || \
+    (__HardenedBSD_version < 1100000 && __HardenedBSD_version > 1000048)
 	return pax_elf(curthread, imgp, flags);
 #elif __HardenedBSD_version == 36 || __HardenedBSD_version > 38
 	return pax_elf(imgp, curthread, flags);
